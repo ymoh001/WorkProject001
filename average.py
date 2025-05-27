48709e795965162423f77910e006e9302a22953e
Yasmin Z <yasmin.mohdzaifullizan@dyson.com>
2025-05-27 13:22:13 +0800

    Your commit message here

--- average.py
+++ average.py
@@ -1,12 +1,15 @@
+# %%
 # function that computes the average of a list of numbers
 def average(numbers):
     return sum(numbers) / len(numbers)
 
+# %%
 #implement the function using a splitted list from the input of user
 def main():
     numbers = [float(i) for i in input("Enter numbers separated by spaces: ").split()]
     print("The average is:", average(numbers))
 
+# %%
 # call the main function
 if __name__ == "__main__":
     main()
